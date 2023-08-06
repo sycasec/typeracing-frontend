d4a40b7d3f74456c5311130b948948a946619d7d
sycasec <sycasec@arch.btw>
2023-08-06 22:42:03 +0800

    barebones, first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import LeaderboardPage from './Pages/LeaderboardPage';
+
+function App() {
+
+  return (
+    <div className="App">
+        <LeaderboardPage/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Pages/LeaderboardPage.tsx
@@ -0,0 +1,10 @@
+import './LeaderboardPage.css';
+
+const LeaderboardPage: React.FC = () => {
+    return (
+        <div className="lb-page-container">
+        </div>
+    );
+};
+
+export default LeaderboardPage;
