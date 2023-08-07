e2a4a9c5932d38d2e093a04d6de8e5217d4d8249
sycasec <kadelcastillo@up.edu.ph>
2023-08-07 15:41:35 +0800

    working temporary table with ant design

--- src/Components/Leaderboard.tsx
+++ src/Components/Leaderboard.tsx
@@ -2,6 +2,8 @@ import Player from '../Types/Player';
 
 import './Leaderboard.css'
 
+import type { ColumnsType } from 'antd/es/table';
+
 import { Table } from 'antd';
 import dayjs, { Dayjs } from 'dayjs';
 import { useEffect } from 'react';
@@ -9,11 +11,41 @@ import { useEffect } from 'react';
 
 const Leaderboard: React.FC = () => {
 
- 
+    const tempData: Player[] = [
+        {
+            ID: 0,
+            Name: 'Kyle',
+            Score: 99.99,
+            Accuracy: 69.69,
+            DateTime: dayjs() 
+        }
+    ]
+
+    const columns: ColumnsType<Player> = [
+        {
+            title: "Name",
+            dataIndex: "Name",
+            key: "Name",
+        },
+        {
+            title: "Score",
+            dataIndex: "Score",
+            key: "Score",
+        },
+        {
+            title: "Accuracy",
+            dataIndex: "Accuracy",
+            key: "Accuracy",
+        }
+    ]
 
     return(
         <div className="leaderboard-container">
-            
+            < Table<Player> 
+                columns={columns} 
+                dataSource={tempData} 
+                pagination={{hideOnSinglePage:true}}
+            />
         </div>
     );
 };
